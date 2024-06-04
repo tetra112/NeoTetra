074a561bc5b78c2043c47219388975bdd9a0b447
tetra112 <55913523+tetra112@users.noreply.github.com>
2024-06-04 13:12:14 +0200

    abc
    neste: kollisjon med target

--- spill/breakout/egetSpill/breakoutScript.js
+++ spill/breakout/egetSpill/breakoutScript.js
@@ -1,3 +1,9 @@
+/* 
+    Neste ting som skal gjøres er å lage kollisjonskode mellom ball og target
+    se linje: 140
+*/
+
+
 // variabler ---------------------------------------
     const gameBoardV = {
         width:800,
@@ -7,20 +13,27 @@
         width:120,
         height:20,
         color:'blue',
-        startPos:[(gameBoardV.width/2-(120/2)),10]
+        startPos:[(gameBoardV.width/2-(120/2)),10],
+        pos:[]
     }
     const targetV = {
         width:80,
         height:20,
         color:'aqua',
-        antall: 8
+        antall: 8,
+        rekker: 4
     }
     const ballV = {
         diameter:50,
         color:'red',
-        borderRadius:'50%'
+        borderRadius:'50%',
+        startPos:[(gameBoardV.width/2-(50/2)),50],
+        pos:[],
+        xVector: 2,
+        yVector: 2
     }
-    console.log('test')
+    let timer;
+    const tickRate = 30;
 // Board --------------------------------------------
     let gameBoardEl = document.querySelector('#gameBoard')
         gameBoardEl.style.width = `${gameBoardV.width}px`;
@@ -28,15 +41,32 @@
 
 // Player -------------------------------------------
     let playerEl = document.createElement('div')
+    playerV.pos = playerV.startPos;
         
     genPlayer =()=>{
         playerEl.style.backgroundColor =playerV.color;
         playerEl.style.position = 'absolute';
         playerEl.style.width    = `${playerV.width}px`;
         playerEl.style.height   = `${playerV.height}px`;
-        playerEl.style.left     = `${playerV.startPos[0]}px`;
-        playerEl.style.bottom   = `${playerV.startPos[1]}px`;
         gameBoardEl.appendChild(playerEl)
+    }
+    drawPlayer=()=>{
+        playerEl.style.left     = `${playerV.pos[0]}px`;
+        playerEl.style.bottom   = `${playerV.pos[1]}px`;
+    }
+    movePlayer=(e)=>{
+        switch(e.key){
+            case 'ArrowLeft':
+                if(playerV.pos[0]>0){
+                    playerV.pos[0]-=10;
+                    drawPlayer()
+                }break
+            case 'ArrowRight':
+                if(playerV.pos[0]<(gameBoardV.width-playerV.width)){
+                    playerV.pos[0]+=10;
+                    drawPlayer()
+                }break 
+        }
     }
 // Targets ------------------------------------------
     class TargetC {
@@ -48,17 +78,75 @@
         }
     }
     const targetArr = [];
-    genTargetArr =()=>{
+    genTarget =(x,yPos)=>{
         for (i=0;i<targetV.antall;i++){
-            let xPos = 10+100*i;
+            let xPos = x+100*i;
+            let toppTargetEl = document.createElement('div')
+            toppTargetEl.style.position = 'absolute';
+            toppTargetEl.style.width    = `${targetV.width}px`;
+            toppTargetEl.style.height   = `${targetV.height}px`
+            toppTargetEl.style.left     = `${xPos}px`;
+            toppTargetEl.style.bottom   = `${yPos}px`;
+            toppTargetEl.classList = 'targetS';
+            gameBoardEl.appendChild(toppTargetEl)
+            
             targetArr.push(
-                new TargetC(xPos,370)
+                new TargetC(xPos,yPos)
             )
         }
     }
-    genTargetArr()
-    console.log(targetArr)
+    printTarget = () => {
+        genTarget(10,370)
+        genTarget(10,330)
+        genTarget(10,290)
+        genTarget(10,250)
+    }
 // Ball ---------------------------------------------
+    ballV.pos = ballV.startPos;
+    let ballEl = document.createElement('div')
+        ballEl.classList = 'ballS'
+        ballEl.style.width = `${ballV.diameter}px`
+        ballEl.style.height = `${ballV.diameter}px`
+        gameBoardEl.appendChild(ballEl)
+    drawBall=()=>{
+        ballEl.style.left   = `${ballV.pos[0]}px`
+        ballEl.style.bottom = `${ballV.pos[1]}px`
+    }
+    ballVector =()=>{
+        ballV.pos[0]+=ballV.xVector
+        ballV.pos[1]+=ballV.yVector
+        collision()
+        drawBall()
+    }
+// Kollisjon ----------------------------------------
+    collision =()=>{
+        // Vegg
+        if  (ballV.pos[0]<0 || 
+            (ballV.pos[0]+ballV.diameter)>gameBoardV.width){
+                ballV.xVector = ballV.xVector*-1;
+            }
+        if  (ballV.pos[1]<0 || 
+            (ballV.pos[1]+ballV.diameter)>gameBoardV.height){
+                ballV.yVector = ballV.yVector*-1;
+        }
+        // player
+        if ((ballV.pos[0]+ballV.diameter)>playerV.pos[0] && 
+            ballV.pos[0]<(playerV.pos[0]+playerV.width) &&
+            ballV.pos[1]<(playerV.pos[1]+playerV.height)
+            ){
+                ballV.yVector = ballV.yVector*-1;
+            }
+        // target
+        
+    }
 
 // Funksjoner ---------------------------------------
     genPlayer()
+    printTarget()
+    drawPlayer()
+    document.addEventListener('keydown',movePlayer)
+    timer = setInterval(ballVector,tickRate)
+
+/*
+
+*/
